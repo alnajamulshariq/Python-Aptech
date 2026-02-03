ef5264e28a2274634cf4c19109fa7c74b06ef7d8
ShariqNajam <mschariquen@gmail.com>
2026-02-03 07:14:37 +0500

    python

---
+++ session-1/modules.py
@@ -0,0 +1,8 @@
+import os
+import calendar
+from playsound import playsound
+
+print(os.listdir())
+print(calendar.month(2024, 1))
+
+playsound(r"session-1\sound-samples\dark-engine-logo-141942.mp3")
