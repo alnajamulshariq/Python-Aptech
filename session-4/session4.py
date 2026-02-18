00276b9130efe9882045d99bb2576899f6afe40e
ShariqNajam <mschariquen@gmail.com>
2026-02-18 10:50:16 +0500

    session 4 list methods and dictionary

---
+++ session-4/session4.py
@@ -0,0 +1,118 @@
+# my_list = [1, 2, 3, 4, 5]
+# print(my_list[ : -2])
+
+
+#modification of list
+# my_list = [1, 2, 3, 4, 5]
+# my_list[2]  = 10
+# print(my_list)
+
+
+# append an element
+# my_list = [1, 2, 3, 4, 5]
+# my_list.append(6)
+# print(my_list)
+
+
+# insert multiple elements in list
+# my_list = [1, 2, 3, 4, 5]
+# my_list.extend([6, 7, 8])
+# print(my_list)
+
+
+# insert an element at specific index
+# my_list = [1, 2, 3, 4, 5]
+# my_list.insert(2, 10)
+# print(my_list)
+
+
+# remove an element from list
+# my_list = [1, 2, 3, 4, 5]
+# my_list.remove(3)
+# print(my_list)
+
+
+#delete an element from list using index
+# my_list = [1, 2, 3, 4, 5]
+# del my_list[2]
+# print(my_list)
+
+
+
+# dictionary
+
+# countryCapital = {
+#     "Pakistan": "Islamabad",
+#     "India": "New Delhi",
+#     "Bangladesh": "Dhaka",
+#     "Afghanistan": "Kabul",
+#     "Saudi Arabia": "Riyadh"
+# }
+
+# print(countryCapital)
+# print(type(countryCapital))
+# print(len(countryCapital))
+
+
+# Accessing values from dictionary
+# countryCapital = {
+#     "Pakistan": "Islamabad",
+#     "India": "New Delhi",
+#     "Bangladesh": "Dhaka",
+#     "Afghanistan": "Kabul",
+#     "Saudi Arabia": "Riyadh"
+# }
+# print(countryCapital["Pakistan"])
+
+# countryCapital["Pakistan"] = "Karachi"
+# print(countryCapital["Pakistan"])
+
+
+# kya dictionary mein key ko update kar sakte hain?
+# countryCapital = {
+#     "Pakistan": "Islamabad",
+#     "India": "New Delhi",
+#     "Bangladesh": "Dhaka",
+#     "Afghanistan": "Kabul",
+#     "Saudi Arabia": "Riyadh"
+# }
+
+# countryCapital["Pakistan"] = countryCapital["USA"]
+# print(countryCapital)
+
+#kya hum apni key ko duplicate kar sakte hain?
+# countryCapital = {
+#     "Pakistan": "Islamabad",
+#     "India": "New Delhi",
+#     "Bangladesh": "Dhaka",
+#     "Afghanistan": "Kabul",
+#     "Saudi Arabia": "Riyadh",
+#     "Pakistan": "Karachi"
+# }
+# print(countryCapital)
+
+
+#clear method in dictionary
+# countryCapital = {
+#     "Pakistan": "Islamabad",
+#     "India": "New Delhi",
+#     "Bangladesh": "Dhaka",
+#     "Afghanistan": "Kabul",
+#     "Saudi Arabia": "Riyadh"
+# }
+# countryCapital.clear()
+# print(countryCapital)
+
+
+
+# stdMarks = {
+#     "Ali": 85,
+#     "Ahmed": 90,
+#     "Sara": 95,
+#     "Zain": 80
+# }
+
+# print(stdMarks)
+# print(stdMarks.keys())
+# print(stdMarks.values())
+
